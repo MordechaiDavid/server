10ff71216509fc4866666fe208f325c084b2aee8
chagaichaimi <chagaichaimi.com>
2024-05-14 22:42:17 +0300

    add

--- src/main/java/com/ashcollege/utils/Utils.java
+++ src/main/java/com/ashcollege/utils/Utils.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -19,22 +20,37 @@ public class Utils {
 
     @PostConstruct
     public void createTeams() {
-        String[] teamNames = {"Barcelona FC", "Real Madrid FC", "Milan AC", "PSG FC", "liverpool FC", "Arsenal FC", "Manchester United FC", "Macabi TA FC"};
         List<Team> teams = persist.loadTeams(Team.class);
-        if (teams.isEmpty())
+        if (teams.isEmpty()) {
+            String[] teamNames = {"Barcelona FC", "Real Madrid FC", "Milan AC", "PSG FC", "liverpool FC", "Arsenal FC", "Manchester United FC", "Macabi TA FC"};
             for (int i = 0; i < 8; i++) {
                 Team team = new Team(teamNames[i]);
                 persist.save(team);
             }
+        }
         List<Match> matchList = persist.loadMatches(Match.class);
         if(matchList.isEmpty()) {
+            List<Date> dateList = new ArrayList<>();
+            Date today = new Date();
+            Calendar calendar = Calendar.getInstance();
+            for (int i = 0; i < 28; i++) {
+                calendar.setTime(today);
+                calendar.set(Calendar.HOUR_OF_DAY, 20);
+                calendar.set(Calendar.MINUTE, 0);
+                calendar.set(Calendar.SECOND, 0);
+                calendar.add(Calendar.DAY_OF_MONTH, i * 2);
+                Date currentDate = calendar.getTime();
+                dateList.add(currentDate);
+            }
+            int count = 0;
 
-            // create 27 matches
+
             List<Match> matches = new ArrayList<>();
             for (int i = 1; i <= 8; i++) {
                 for (int j = i + 1; j <= 8; j++) {
-                    Match match = new Match(i, j, new Date(), j - 1);
+                    Match match = new Match(i, j, dateList.get(count), j - 1);
                     matches.add(match);
+                    count++;
                 }
             }
             for (Match match : matches) {
